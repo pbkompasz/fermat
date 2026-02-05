0c04b6377dd6128b9d1f83e4b11050d479bc4741
Peter Kompasz <kompaszbence@gmail.com>
2026-02-06 01:17:14 +0200

    feat: change layout

--- src/frontend/src/components/Main.tsx
+++ src/frontend/src/components/Main.tsx
@@ -12,8 +12,38 @@ const Main = () => {
         <h2>Main</h2>
       </div>
       <div>
-        <p>Status: {userStatus}</p>
+        <p>User Status: {userStatus}</p>
         <p>Agent Status: {agentStatus}</p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          height: "200px"
+        }}
+      >
+        <div
+          style={{
+            borderRight: "solid 1px",
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h3>TODO(Short Term)</h3>
+        </div>
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h3>Long Term</h3>
+        </div>
       </div>
     </>
   );
