5b48b6087d732ec54037c66cc2faca28b8d4436f
Peter Kompasz <kompaszbence@gmail.com>
2026-02-06 01:16:42 +0200

    feat: reference demo device

--- src/frontend/src/components/Device.tsx
+++ src/frontend/src/components/Device.tsx
@@ -1,14 +1,38 @@
-const Device = () => {
+import { useState } from "react";
+import Modal from "./Modal";
+import DemoDevice from "./DemoDevice";
+
+const Device = ({ isDemo = false }: { isDemo: boolean }) => {
   const linkDevice = () => {};
+  const [showDemoDevice, setShowDemoDevice] = useState(false);
 
   return (
-    <>
+    <div style={{}}>
       <div className="subsection--title">
         <h2>Device</h2>
-        <button onClick={linkDevice}>Link device(s)</button>
+        <div>
+          {isDemo ? (
+            <>
+              <button
+                onClick={() => setShowDemoDevice(true)}
+                style={{ marginRight: "1rem" }}
+              >
+                View demo device
+              </button>
+              <Modal
+                isOpen={showDemoDevice}
+                setIsOpen={setShowDemoDevice}
+                title="Demo Device"
+                content={<DemoDevice />}
+              />
+            </>
+          ) : (
+            <button onClick={linkDevice}>Link device(s)</button>
+          )}
+        </div>
       </div>
       <div>connected devices, but really the fs(sshfs for the demo)</div>
-    </>
+    </div>
   );
 };
 
