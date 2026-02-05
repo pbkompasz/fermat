6e5707f11872ccb9e6a18489e72fb5daf4272f21
Peter Kompasz <kompaszbence@gmail.com>
2026-02-05 20:20:39 +0200

    feat: add props

--- src/frontend/src/components/Modal.tsx
+++ src/frontend/src/components/Modal.tsx
@@ -1,20 +1,38 @@
+import type { ReactNode } from "react";
 import ReactModal from "react-modal";
 
 const Modal = ({
   title,
   isOpen,
   setIsOpen,
+  content,
 }: {
-  title: string;
+  title?: string;
   isOpen: boolean;
   setIsOpen: (opt: boolean) => void;
+  content: ReactNode;
 }) => {
   return (
     <>
-      <ReactModal isOpen={isOpen}>
-        {title}
-        <p>Modal Content</p>
-        <button onClick={() => setIsOpen(false)}>close</button>
+      <ReactModal
+        isOpen={isOpen}
+        style={{ content: { backgroundColor: "#242424", padding: "2rem" } }}
+      >
+        <div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <h2>{title ?? ""}</h2>
+            <button onClick={() => setIsOpen(false)} style={{ height: "3rem" }}>
+              Close
+            </button>
+          </div>
+          {content ?? "Empty"}
+        </div>
       </ReactModal>
     </>
   );
